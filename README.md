fc3f9c1f7de1424ead8439e0bc207fd1b37948ba
yperera20 <buddhinperera@yahoo.com>
2023-11-07 02:08:55 +1100

    update4

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Note-Taker-00112
+# Note Taker Application
+
+The Note Taker application is a simple web-based note-taking tool built with Node.js and Express. It allows users to create, save, and delete notes in a straightforward interface.
+
+
+## Features
+
+- Create and save notes with a title and text.
+- View and manage saved notes.
+- Delete notes you no longer need.
+- Simple and user-friendly interface.
+- Backend data storage using `db.json`.
+
+## Getting Started
+
+Follow these instructions to get a copy of the project up and running on your local machine.
+
+### Prerequisites
+
+To run this application, you need to have [Node.js](https://nodejs.org/) installed on your computer.
+
+### Application
+
+Link - 
+
--- server.js
+++ server.js
@@ -1,23 +1,67 @@
 const express = require('express');
-const sqlite3 = require('sqlite3');
-const app = express();
-const port = process.env.PORT || 3000;
+const fs = require('fs');
+const path = require('path');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON data in requests
 app.use(express.json());
 
-// Connect to the SQLite database
-const db = new sqlite3.Database('./db/notes.db');
-
-// Create a table to store notes (you may need to adjust the schema).
-db.serialize(() => {
-  db.run("CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY, title TEXT, text TEXT)");
-});
-
-// Serve static files from the "public" folder
+// Serve static files from a 'public' directory
 app.use(express.static('public'));
 
-// Define API endpoints below
+// HTML Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// API Routes
+app.get('/api/notes', (req, res) => {
+  // Read notes from db.json
+  fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) {
+      res.json([]);
+    } else {
+      const notes = JSON.parse(data);
+      res.json(notes);
+    }
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  // Read existing notes from db.json
+  fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      const notes = JSON.parse(data);
+
+      // Create a new note
+      const newNote = {
+        id: Math.floor(Math.random() * 1000), // Generate a unique ID (you can use a library for better unique IDs)
+        title: req.body.title,
+        text: req.body.text,
+      };
+
+      notes.push(newNote);
+
+      // Write updated notes to db.json
+      fs.writeFile('db.json', JSON.stringify(notes), (err) => {
+        if (err) {
+          res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+          res.json(newNote);
+        }
+      });
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
 });
